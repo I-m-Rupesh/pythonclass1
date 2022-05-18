b995afba08329fb3666cc06166ed70bc26bafc90
Rupesh Mishra <rupesh.shrikant.mishra@gmail.com>
2022-05-18 14:33:54 +0530

    Best of n game logic added in game 1 file

--- game1.py
+++ game1.py
@@ -1,14 +1,15 @@
+
 import random as rd
 
 print('Welcome to evodd')
 
 game = True
-
+uscore = 0
+bscore = 0
 while game:
-
     choice = input('Select even or odd\n')
     mynum = int(input("My turn "))
-    print('bot turn')
+    print('\n\nbot turn')
     botnum = rd.randint(1,10)
     print('Bot selected ', botnum)
     add = mynum + botnum
@@ -20,13 +21,20 @@ while game:
         ans = 'odd'
 
     if choice == ans:
+        uscore += 1
         print('You won!!!\n\n')
     else:
+        bscore += 1
         print('Bot won!!!\n\n')
 
-    ch=int(input('Press "1" to continue !!'))
-    
+    ch = int(input('Enter "1" to continue !!'))
     if ch != 1:
         game = False
-        print("Thank you!")
+        if uscore > bscore:
+            print("Player Won the game")
+        elif uscore < bscore:
+            print("Bot Won the game")
+        else:
+            print("Game Draw")
+        print("Thank you for playing!!")
     
