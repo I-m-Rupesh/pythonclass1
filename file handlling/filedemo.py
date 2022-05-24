73561b1362589f8d10c4f3e81e820476516e03d3
Rupesh Mishra <rupesh.shrikant.mishra@gmail.com>
2022-05-24 12:00:43 +0530

    file handlling code modified

--- file handlling/filedemo.py
+++ file handlling/filedemo.py
@@ -2,17 +2,21 @@
 # file modification
 # file close
 
+# to create file 
+#f = open('sample.txt', 'x')  
 
-
+# to write content in the file
 f = open('sample.txt', 'w')
 content = input('Enter text to write in file')
 f.write(content)
 f.close()
 
+# to read  content from the file
 f = open('sample.txt', 'r')
 print(f.read())
 f.close()
 
+# to append content in the file
 f = open('sample.txt', 'a')
 content = input('Enter text to write in file')
 f.write(content)
