515c17f1aa009152a14a9bd4d31aa0f4464d7178
Rupesh Mishra <rupesh.shrikant.mishra@gmail.com>
2022-06-18 06:08:18 +0530

    dictionary example modified

--- dictdemo.py
+++ dictdemo.py
@@ -1,25 +1,36 @@
 # key : value
 
-mydetails = {'name': 'rupesh', 'subject': 'python', 'age':30}
+def displayDict(d):
+    for i in d:
+        print(d[i])
 
-mydetails.pop('age')
-for x, y in mydetails.items():
-  print(x, y)
-
-
-#print(mydetails)
+# days = {}
+# print(type(days))
 # no repetation
-# change disctionary 
-# mydetails['city'] = 'mumbai'
-# mydetails['city'] = 'surat'
+days = {'day1': 'Sunday', 'day2':'Monday', 'day3':'Tuesday' }
+displayDict(days)
+# print('After adding new day')
+days['day4'] = 'Wednesday'
+# displayDict(days)
+# print('After adding wrong day')
+# days['day4'] = 'Wrongday'
+# displayDict(days)
+# print('After removing wrong day')
+# days.pop('day4')
+# displayDict(days)
 
-# print(mydetails)
+# Display only values
+print(days.values())
+# Display only keys
+print(days.keys())
+# display total number of element
+print(len(days))
 
-# k = mydetails.keys()
-# print(k)
+#check sunday is present in dict
+print('Sunday' in days.values())
 
-# v = mydetails.values()
-# print(v)
+#check day2 is present in dict
+print('day2' in days)
 
-# for i in mydetails:
-#     print(mydetails[i])
+for i,j in zip(days.keys(),days.values()):
+    print(i,'is',j)
---
+++ dictdemo1.py
@@ -0,0 +1,25 @@
+# key : value
+
+mydetails = {'name': 'rupesh', 'subject': 'python', 'age':30}
+
+mydetails.pop('age')
+for x, y in mydetails.items():
+  print(x, y)
+
+
+#print(mydetails)
+# no repetation
+# change disctionary 
+# mydetails['city'] = 'mumbai'
+# mydetails['city'] = 'surat'
+
+# print(mydetails)
+
+# k = mydetails.keys()
+# print(k)
+
+# v = mydetails.values()
+# print(v)
+
+# for i in mydetails:
+#     print(mydetails[i])
